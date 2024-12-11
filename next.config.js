7adc59db52983a020f18c6e2a1b61cbec46f5653
ndrd79 <ndrd7980@gmail.com>
2024-12-11 16:44:06 -0300

    feat: adicionar configurações de ambiente para produção

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  }
 }
 
 module.exports = nextConfig
